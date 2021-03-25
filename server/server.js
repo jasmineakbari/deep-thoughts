79e5c2a3bb0b1d44261a344f2a5675a14cf627c2
jasmineakbari <jasmineakbari@yahoo.com>
2021-03-24 22:30:26 -0400

    set up start command from root directory

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
@@ -19,6 +20,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
